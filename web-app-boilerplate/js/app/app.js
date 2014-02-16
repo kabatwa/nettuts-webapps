4a0cde51d0cb058da1a4d9ce3c507a0183bd83ec
Timothy Bryan <tim@orangesauce.org>
2014-02-16 12:02:39 +0000

    Finished Setting Up Models/Collections *Not Working*

--- web-app-boilerplate/js/app/app.js
+++ web-app-boilerplate/js/app/app.js
@@ -1,18 +1,33 @@
 define([
-    'app/views/app',
-    'app/routers/router'
-], function(AppView,Router){
-    'use strict';
+  'app/views/app',
+  'app/routers/router',
+  'app/models/app',
+  'app/collections/days'
+], function(AppView,Router,AppModel,DaysCollection){
+  'use strict';
 
-    var initialize=function(){
-      var appView=new AppView();
-      $('body').append(appView.el);
-      var router=new Router(appView);
-      Backbone.history.start();
+  var initialize=function(){
+    var appModel=new AppModel();
+    var appView=new AppView({model:appModel});
+    $('body').append(appView.render().el);
+    var router=new Router(appView);
+    Backbone.history.start();
 
-    };
+    var daysCollection=new DaysCollection([],{
+      url:'http://api.wunderground.com/api/d48b724aca90503f/forecast/q/UK/Newcastle.json'
+    });
 
-    return{
-      initialize:initialize
-    }
+    daysCollection.fetch({
+      success:function(collection,response,options){
+        console.log(collection,response);
+      },
+      error:function(collection,response,options){
+        console.log('error');
+      },
+    });
+  };
+
+  return{
+    initialize:initialize
+  }
 });
---
+++ web-app-boilerplate/js/app/models/app.js
@@ -0,0 +1,15 @@
+define([
+  'backbone'
+],function(Backbone){
+  'use strict';
+
+  var AppModel=Backbone.Model.extend({
+    defaults:{
+      'backgroundColor':'#999999',
+      'celsius':true,
+      'welcomeMessage':'Welcome to Weather Watcher'
+    }
+  });
+
+  return AppModel;
+});
